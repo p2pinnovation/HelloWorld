4c153c3f747ca46d4b5e6b85b7eefb66cdbe9e34
p2pinnovation <bsappdev101@gmail.com>
2020-06-24 18:52:11 +0800

    label updated

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,8 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println(" ***** Welcome to My App World  for Jenkins! *******");
-		System.out.println(" ***** Automate Continuous Integration with Jenkins & Github! *******");
+        System.out.println(" ***** Welcome to My Java App World  for Jenkins! *******");
+		System.out.println(" >>>> Automate Continuous Integration with Jenkins & Github! >>>>");
     }
 
 }
